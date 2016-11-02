51e4c23269115912699abe3a854419b6307a87b0
Lars Girndt <lars.girndt@brands4friends.de>
2016-11-02 10:34:04 +0100

    Get java working

---
+++ src/integration/java/de/girndt/ExampleTest.java
@@ -0,0 +1,12 @@
+package de.girndt;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+
+   @Test
+   public void test_something() {
+      Assert.assertTrue(true);
+   }
+}

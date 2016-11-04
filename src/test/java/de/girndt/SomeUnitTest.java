ef355c954c3297d15982d58b81b02ef29e56b10d
Lars Girndt <lars.girndt@brands4friends.de>
2016-11-04 17:07:51 +0100

    Add a unit test

---
+++ src/test/java/de/girndt/SomeUnitTest.java
@@ -0,0 +1,12 @@
+package de.girndt;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SomeUnitTest {
+
+    @Test
+    public void test_something_in_this_unit_test() {
+        Assert.assertTrue(true);
+    }
+}

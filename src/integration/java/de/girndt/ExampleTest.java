cbd8eae6c91fa694da4659851597fdfcbba7c332
Lars Girndt <lars.girndt@brands4friends.de>
2016-11-04 17:00:15 +0100

    Get integration tests running

--- src/integration/java/de/girndt/ExampleTest.java
+++ src/integration/java/de/girndt/ExampleTest.java
@@ -1,12 +1,31 @@
 package de.girndt;
 
+import io.restassured.RestAssured;
+import io.restassured.config.RestAssuredConfig;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.when;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class ExampleTest {
 
-   @Test
-   public void test_something() {
-      Assert.assertTrue(true);
-   }
+    @BeforeClass
+    public static void setupClass() {
+        RestAssured.baseURI = "http://" + System.getenv("INTEGRATION_HOST_PORT");
+    }
+
+    @Test
+    public void test_something() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void get_root_should_be_ok() {
+        when().get("/").then().statusCode(200);
+    }
 }
